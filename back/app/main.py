0860e69baae1c6bde0719fa1fa174ee1c91ae9de
Gregg <arkhipov.zena@gmail.com>
2025-04-09 22:59:16 +0500

    fix: little fix of platform_photo methode

--- back/app/main.py
+++ back/app/main.py
@@ -37,7 +37,7 @@ def get_platforms():
 @app.get("/platform_photo/{platform_id}")
 def read_platform_photo(platform_id: int):
     result = model(f"app/photo/{platform_id}.jpg")
-    return result
+    return result[0].show()
 
 
 @app.get("/platform_info/{id}")
